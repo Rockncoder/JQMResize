5cc8b6a9c072567e7713a3743cd238a34424adda
Troy Miles <rockncoder@gmail.com>
2012-08-03 13:12:54 -0700

    Fixed error in height code.

--- scripts/code.js
+++ scripts/code.js
@@ -5,13 +5,15 @@ var $, navigator, window;
 function getDimensions() {
 	// the iphone specific code is kind of kludgy, if you have a better way let me know
 	var isIPhone = (/iphone/gi).test(navigator.appVersion),
+		iPhoneHeight = (isIPhone ?  60 : 0),
 		width = $(window).width(),
-		height = $(window).height() + (isIPhone ?  60 : 0),
-		hHeight = $('header').outerHeight(),
-		fHeight = $('footer').outerHeight();
+		height = $(window).height(),
+		// if one of these doesn't exist, assign 0 rather a null or undefined
+		hHeight = $('header').outerHeight() || 0,
+		fHeight = $('footer').outerHeight() || 0;
 	return {
 		width: width - 4,
-		height: height - hHeight - fHeight - 4
+		height: height - hHeight - fHeight - 4 + iPhoneHeight
 	};
 }
 
@@ -26,7 +28,7 @@ function reSizeDiv() {
 
 // we are watching all three of these events, if any occur we re-determine the size
 // and scroll the window back to the top
-$(window).bind('resize orientationchange pageinit', function (event) {
+$(window).bind('resize orientationchange pageshow', function (event) {
 	window.scrollTo(1, 0);
 	reSizeDiv();
 });
